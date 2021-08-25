cb4179127eed33f9cf94c6de109d50b08b4b0539
36senm <63abbym@gmail.com>
2021-08-25 12:11:06 +0700

    day 8 or  9 idk

--- praktek_day2.R
+++ praktek_day2.R
@@ -4,8 +4,18 @@ install.packages("readxl")
 install.packages("rJava")
 install.packages("RWeka")
 install.packages("usethis")
+install.packages("measurements")
+install.packages("htmlwidgets")
+install.packages("nycflights13")
+install.packages("devtools")
+
+library(measurements)
 library(tidyverse)
 library(xlsx)
+library(htmlwidgets)
+library(nycflights13)
+library(haven)
+library(keData)
 df_1 <- read.csv(file = "datamarket_or.csv" , sep = ",") #first attempt
 df_2 <- read_csv2(file = "datamarket_or.csv") #akses otomatis separator, jadi tidak bisa menggunakan separator
 
@@ -17,12 +27,10 @@ list_ppl <- read.xlsx(file =  "SEAriously Awesome People List - Startup COVID-19
 #tugas7
 install.packages("devtools")
 devtools::install_github("eppofahmi/keData")
-library(keData)
 daten <- keData ::dataAnime
 dataku <- daten $`Starting season` 
 
 #import stata
-library(haven)
 df_salary <- read_dta(file = "data/salary.dta")
 #import spss
 df_bike = read_sav(file = "data/PsychBike.sav")
@@ -34,11 +42,11 @@ df_surveyhh = read_sas(data_file = "data/hhsurvey.sas7bdat")
 str(df_bike)
 glimpse(df)
 
-install.packages("nycflights13")
-library(nycflights13)
+
 
 df_flights = flights
 ?flights
+View(df_flights)
 
 #filter month&date based
 filter(flights, month == 3, day == 6)
@@ -54,5 +62,86 @@ df_filter12 = filter(flights, month == 2 | month == 6, day <= 30 )
 
 #konsep pipeline dalam filter
 df_filter_ppline = df_flights %>% 
-  filter(month == 1 | month == 2)
+  filter(month == 1 | month == 2) %>%
+  filter(origin == "EWR")
   
+#arrange()
+df_filter_ord <- df_filter12 %>%
+  arrange(origin)
+
+df_filter_delayed <- df_flights %>%
+  arrange(desc(dep_delay))
+
+  df_filter_earlytake <- df_flights %>%
+  arrange(dep_delay)
+
+#select()
+df_slcflight <- df_flights %>%
+  select(1:10)
+
+#mutate()
+df_hour_airtime <- df_flights %>%
+  mutate(air_time_hour = air_time/60)
+
+df_distance_km <- df_flights %>%
+  mutate(distancekm = distance * 1.60934 ) %>%
+  arrange(desc(distancekm))
+
+#summ
+df_flights %>%
+  group_by(day, month, year) %>%
+  summarize(delay = mean(dep_delay, na.rm= TRUE))
+
+by_origin <- group_by(df_distance_km, origin, dest)
+df_summa <- summarize(by_origin, distance_between = mean(distancekm))
+
+df_sum <- df_distance_km %>%
+  group_by(distancekm, origin, dest) %>%
+  summarize(distancebyfar = mean(distancekm, na.rm = TRUE))
+
+
+#str
+
+df_strflights <- df_distance_km %>% 
+  mutate(string_origin = str_length(origin))
+
+df_flights_carrier <- df_flights %>%
+  mutate(carrier_sub = str_sub(carrier,1,1))
+
+#menggunakan RE untuk transformasi data
+x <- c("apple","banana","anggur","mangga","asem")
+str_view(x,"le")
+#re untuk string awalan
+str_view(x,"^a")
+#re untuk match dengan string akhiran
+str_view(x, "a$")
+
+words <- stringr::words
+str_view(words,"o$")
+filter(words == "o$")
+
+#re untuk match angka 
+kata1 <- "33 juta penghuni lapas"
+str_view(kata1, "\\d")
+
+#re untuk match spasi
+str_view(kata1, "\\s")
+
+#re untuk match lebih dari satu value character
+str_view(x, "[abm]")
+
+#re untuk match kecuali karakter berikut
+str_view(x,"[^amb]")
+
+#re repetisi
+# 0 / 1 : ?
+# 1 atau lebih : +
+# 0 atau lebih : *
+
+xx1 <- "1888 is the longest year in Roman numerals: MDCCCLXXXVIII"
+str_view(xx1, "CC+")
+str_view(xx1, 'C[LX]+')
+str_view(xx1, "CC?")
+str_view(xx1, "p[no]+")
+
+
